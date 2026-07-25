1e044da69284a73a6eb1957e63032fbce9ff7a27
= <sojwalpatil2006@gmail.com>
2026-07-25 14:22:44 +0530

    responsive menu

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const menuBar = document.querySelector(".menu-bar");
+const menuIcon = menuToggle.querySelector("i");
+
+menuToggle.addEventListener("click", () => {
+    menuBar.classList.toggle("active");
+
+    if(menuBar.classList.contains("active")){
+        menuIcon.classList.remove("fa-bars");
+        menuIcon.classList.add("fa-xmark");
+    }else{
+        menuIcon.classList.remove("fa-xmark");
+        menuIcon.classList.add("fa-bars");
+    }
+});
+
 let lastScroll = 0;
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", () => {
+
+    if(menuBar.classList.contains("active")) return;
+
     const currentScroll = window.pageYOffset;
 
-    // Always show at the top
-    if (currentScroll <= 0) {
+    if(currentScroll <= 0){
         header.classList.remove("hide");
         return;
     }
 
-    // Scrolling down
-    if (currentScroll > lastScroll) {
+    if(currentScroll > lastScroll){
         header.classList.add("hide");
-    }
-    // Scrolling up
-    else {
+    }else{
         header.classList.remove("hide");
     }
 
